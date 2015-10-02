34a5495387910c9863dd13dbd39361920058370b
alvaro <alvarotuso@hotmail.com>
2015-10-01 21:24:43 -0300

    setup script

--- gutils/django/scripts/__init__.py
+++
@@ -1,1 +0,0 @@
-from .django_bootstrap import load_django
---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='gutils',
+    version='1.0',
+    packages=['gutils'],
+    include_package_data=True,
+)
